4165e49b1388f9fb83b535b86aba34851556dcba
volp99 <stefanovolpatti@yahoo.it>
2021-05-04 00:10:53 +0200

    create project

---
+++ fetch-api/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import FetchRandomUser from "./FetchRandomUser";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FetchRandomUser/>
+      </header>
+    </div>
+  );
+}
+
+export default App;

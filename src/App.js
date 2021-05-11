9c62b8772637c4e166627c7063d299e7321dd378
volp99 <stefanovolpatti@yahoo.it>
2021-05-11 20:20:24 +0200

    git ignore + radial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
 import  './App.css';
 import FetchRandomUser from "./FetchRandomUser";
-
+import Radial from "./Radial";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <FetchRandomUser/>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <FetchRandomUser/>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
